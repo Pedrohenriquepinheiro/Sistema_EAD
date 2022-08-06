a8914203327dbc44f9a67e379c5eb7eb9efc8079
Pedro Henrique Pinheiro Silva <pedrohenriquevgtp@gmail.com>
2022-08-06 13:44:23 -0300

    Sistema_EAD

--- index.php
+++ index.php
@@ -40,22 +40,7 @@ if(isset($_GET['p'])) {
 
 <body>
     <!-- Pre-loader start -->
-    <div class="theme-loader">
-        <div class="ball-scale">
-            <div class='contain'>
-                <div class="ring"><div class="frame"></div></div>
-                <div class="ring"><div class="frame"></div></div>
-                <div class="ring"><div class="frame"></div></div>
-                <div class="ring"><div class="frame"></div></div>
-                <div class="ring"><div class="frame"></div></div>
-                <div class="ring"><div class="frame"></div></div>
-                <div class="ring"><div class="frame"></div></div>
-                <div class="ring"><div class="frame"></div></div>
-                <div class="ring"><div class="frame"></div></div>
-                <div class="ring"><div class="frame"></div></div>
-            </div>
-        </div>
-    </div>
+
     <!-- Pre-loader end -->
     <div id="pcoded" class="pcoded">
         <div class="pcoded-overlay-box"></div>
--- page/cadastrar_curso.php
+++ page/cadastrar_curso.php
@@ -1,20 +1,60 @@
  <?php
-
 include('lib/conexao.php');
+include('lib/enviarArquivos.php');
 
-if(isset(['enviar'])) {
+if(isset($_POST['enviar'])) {
     
-    $titulo = $_POST['titulo'];
-    $descricao_curta = $_POST['descricao_curta'];
-    $preco = $_POST['preco'];
-    $conteudo = $_POST['conteudo'];
+    $titulo = $mysqli->escape_string($_POST['titulo']);
+    $descricao_curta = $mysqli->escape_string($_POST['descricao_curta']);
+    $preco = $mysqli->escape_string($_POST['preco']);
+    $conteudo = $mysqli->escape_string($_POST['conteudo']);
+
+    $erro = array();
+    if(empty($titulo))
+        $erro[] = "Preencha o título";
+
+    if(empty($descricao_curta))
+        $erro[] = "Preencha a descrição curta";
+
+    if(empty($preco))
+        $erro[] = "Preencha a preco";
+
+    if(empty($conteudo))
+        $erro[] = "Preencha a conteúdo";
+
+    if(!isset($_FILES) || !isset($_FILES['imagem']) || $_FILES['imagem']['size'] == 0)
+        $erro[] = "Selecione uma imagem para o conteúdo";
+
+    if(count($erro) == 0) {
+
+        $deu_certo = enviarArquivos($_FILES['imagem']['error'], $_FILES['imagem']['size'], $_FILES['imagem']['name'], $_FILES['imagem']['tmp_name']);
+        if($deu_certo !== false) {
+
+            $sql_code = "INSERT INTO cursos (titulo, descricao_curta, conteudo, data_cadastro, preco, imagem) 
+            VALUES(
+                '$titulo',
+                '$descricao_curta',
+                '$conteudo',
+                NOW(),
+                '$preco',
+                '$deu_certo'
+            )";
+            $inserido = $mysqli->query($sql_code);
+            if(!$inserido)
+                $erro[] = "Falha ao inserir no banco de dados: " . $mysqli->error;
+            else 
+                die("<script>location.href=\"index.php\";</script>");
+
+        } else 
+            $erro[] = "Falha ao enviar a imagem";
+    }
 }
 
 ?>
  <!-- Page-header start -->
  <div class="page-header card">
     <div class="row align-items-end">
-        <div class="col-lg-8">
+        <div class="col-lg-6">
             <div class="page-header-title">
                 <div class="d-inline">
                     <h4>Cadastrar Curso</h4>
@@ -22,12 +62,17 @@ if(isset(['enviar'])) {
                 </div>
             </div>
         </div>
-        <div class="col-lg-4">
+        <div class="col-lg-6">
             <div class="page-header-breadcrumb">
                 <ul class="breadcrumb-title">
                     <li class="breadcrumb-item">
                         <a href="index.php">
                             <i class="icofont icofont-home"></i>
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item">
+                        <a href="index.php?p=gerenciar_cursos">
+                            Gerenciar Cursos
                         </a>
                     </li>
                     <li class="breadcrumb-item">Cadastrar Curso</li>
@@ -41,6 +86,15 @@ if(isset(['enviar'])) {
 <div class="page-body">
     <div class="row">
         <div class="col-sm-12">
+            <?php if(isset($erro) && count($erro) > 0) {
+                ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php foreach($erro as $e) { echo "$e<br>"; } ?>
+                </div>
+                <?php
+            }
+            ?>
+
             <div class="card">
                 <div class="card-header">
                     <h5>Formulário de Cadastro</h5>
